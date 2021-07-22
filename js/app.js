6ad0255d7509de53dc46bbffc8eaac007a8b3ae4
srisrinu1 <srinivasreddychalla133@gmail.com>
2021-07-22 19:07:45 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -6,4 +6,21 @@ const audio = document.querySelector('#audio');
 const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
 const Title = document.querySelector('#title');
-const cover = document.querySelector('#cover');
+const Cover = document.querySelector('#cover');
+// songs
+const songs = ["Gul", "Hututu", "Param Sundari", "Rihaayi De"];
+
+//keep track of song
+let songIndex = 0;
+console.log(songIndex);
+
+//load song into DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    Cover.src = `images/${song}.jpg`;
+
+}
